9b6081f6a2e3b1c7d795ded7c5e48ec0d30af783
shumo.tokinaga <longtime1116@gmail.com>
2020-01-14 20:09:37 +0900

    Create Dynamic Pages

--- hello-next/pages/index.js
+++ hello-next/pages/index.js
@@ -1,11 +1,35 @@
 import Layout from "../components/Layout";
+import Link from "next/link";
 
-const Index = () => (
+const PostLink = props => (
+  <li>
+    <Link href={`./post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
+const Blog = () => (
   <div>
     <Layout>
-      <p>Hello Next.js</p>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
     </Layout>
   </div>
 );
 
-export default Index;
+export default Blog;
+
+//const Index = () => (
+//  <div>
+//    <Layout>
+//      <p>Hello Next.js</p>
+//    </Layout>
+//  </div>
+//);
+//
+//export default Index;
+//
